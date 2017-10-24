9b7ba1ef02744c6719a132083b936a79288558e4
pedro alonso najar leal <pittnajar@hotmail.com>
2017-10-24 18:32:21 -0500

    Terminado2

--- app/src/main/java/com/example/panaj/myphonecontac/MainActivity.java
+++ app/src/main/java/com/example/panaj/myphonecontac/MainActivity.java
@@ -1,70 +1,71 @@
 package com.example.panaj.myphonecontac;
-        import java.util.ArrayList;
 
-        import android.Manifest;
-        import android.app.Activity;
-        import android.app.ProgressDialog;
-        import android.content.ContentResolver;
-        import android.content.pm.PackageManager;
-        import android.database.Cursor;
-        import android.net.Uri;
-        import android.os.Bundle;
-        import android.os.Handler;
-        import android.provider.ContactsContract;
-        import android.support.v4.app.ActivityCompat;
-        import android.support.v4.content.ContextCompat;
-        import android.view.View;
-        import android.widget.AdapterView;
-        import android.widget.AdapterView.OnItemClickListener;
-        import android.widget.ArrayAdapter;
-        import android.widget.ListView;
-        import android.widget.Toast;
+import java.util.ArrayList;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
     private ListView mListView;
     private ProgressDialog pDialog;
     private Handler updateBarHandler;
+    private final static int MY_PERMISSIONS_REQUEST_READ_CONTACTS= 1;
+    private final static int MY_PERMISSIONS_REQUEST_CALL_PHONE=1;
     ArrayList<String> contactList;
+    ArrayList<String> phonenomber;
     Cursor cursor;
     int counter;
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS=1;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_phone_contac);
-
-// Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
+        if(ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_CONTACTS)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.READ_CONTACTS)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS))
+            {
+                //ALgo
+            }
+            else
+            {
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.READ_CONTACTS},
                         MY_PERMISSIONS_REQUEST_READ_CONTACTS);
 
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
+
         }
+
+
         pDialog = new ProgressDialog(this);
         pDialog.setMessage("Reading contacts...");
         pDialog.setCancelable(false);
         pDialog.show();
-        mListView = (ListView) findViewById(R.id.list_item);
+        mListView = (ListView) findViewById(R.id.list);
         updateBarHandler =new Handler();
         // Since reading contacts takes more time, let's run it on a separate thread.
         new Thread(new Runnable() {
@@ -74,17 +75,49 @@ public class MainActivity extends Activity {
             }
         }).start();
         // Set onclicklistener to the list item.
-        mListView.setOnItemClickListener(new OnItemClickListener() {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 //TODO Do whatever you want with the list data
-                Toast.makeText(getApplicationContext(), "item clicked : \n"+contactList.get(position), Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(), "item clicked : \n"+contactList.get(position), Toast.LENGTH_SHORT).show();
+
+                call(phonenomber.get(position));
             }
         });
     }
+
+    private void call(String tel) {
+
+        try {
+
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:"+tel));
+            System.out.println("====before startActivity====");
+
+
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) !=
+                    PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        MY_PERMISSIONS_REQUEST_CALL_PHONE);
+
+                return;
+            }
+
+            startActivity(callIntent);
+            System.out.println("=====getcallActivity==="+getCallingActivity());
+
+
+        } catch (ActivityNotFoundException e) {
+            Log.e("helloAndroid","Call failed",e);
+        }
+    }
     public void getContacts() {
         contactList = new ArrayList<String>();
+        phonenomber= new ArrayList<String>();
         String phoneNumber = null;
         String email = null;
         Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
@@ -98,6 +131,7 @@ public class MainActivity extends Activity {
         String EmailCONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
         String DATA = ContactsContract.CommonDataKinds.Email.DATA;
         StringBuffer output;
+        StringBuffer phone;
         ContentResolver contentResolver = getContentResolver();
         cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
         // Iterate every contact in the phone
@@ -105,6 +139,7 @@ public class MainActivity extends Activity {
             counter = 0;
             while (cursor.moveToNext()) {
                 output = new StringBuffer();
+                phone= new StringBuffer();
                 // Update the progress message
                 updateBarHandler.post(new Runnable() {
                     public void run() {
@@ -121,6 +156,7 @@ public class MainActivity extends Activity {
                     while (phoneCursor.moveToNext()) {
                         phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
                         output.append("\n Phone number:" + phoneNumber);
+                        phone.append(phoneNumber);
                     }
                     phoneCursor.close();
                     // Read every email id associated with the contact
@@ -133,6 +169,7 @@ public class MainActivity extends Activity {
                 }
                 // Add the contact to the ArrayList
                 contactList.add(output.toString());
+                phonenomber.add(phone.toString());
             }
             // ListView has to be updated using a ui thread
             runOnUiThread(new Runnable() {
